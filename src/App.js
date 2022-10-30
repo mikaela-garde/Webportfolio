2a444c3ee3e9e98cef4fcbbbd27a88c51c56c334
Mikaela Gärde <51963556+mikaela-garde@users.noreply.github.com>
2022-10-30 10:15:11 +0100

    updates to fix bug

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import { Outlet } from 'react-router-dom';
 import './css/App.css';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import {firebaseConfig} from "./firebase-config";
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 function App() {
   return (
---
+++ src/firebase-config.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: "AIzaSyAJY8nG4I7dK1tJGr5PcmhmOxMhYHrW0FY",
+  authDomain: "mikaelagarde-96809.firebaseapp.com",
+  projectId: "mikaelagarde-96809",
+  storageBucket: "mikaelagarde-96809.appspot.com",
+  messagingSenderId: "1001415399442",
+  appId: "1:1001415399442:web:baff10486501673ace60cb",
+  measurementId: "G-EGKE8PFN0B"
+};
